448f866fa2364f981096583edc6092f6b5bf66d7
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2025-09-21 14:02:16 +0530

    added two pointer template

--- dsa-pattern/src/Test.java
+++
@@ -1,8 +0,0 @@
-/**
- * Creator : the-wolverine-coder-UjjwalKumar
- **/
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("Test ujjwal!");
-    }
-}
--- dsa-pattern/src/two_pointer/TwoPointer.java
+++ dsa-pattern/src/two_pointer/TwoPointer.java
@@ -3,7 +3,7 @@ package two_pointer;
 /**
  * Creator : the-wolverine-coder-UjjwalKumar
  **/
-public class TwoPointerUnderstanding {
+public class TwoPointer {
 
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
